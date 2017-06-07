dab6ed5e5373f1590612c459dc3f2aa34c6f56da
Pham Kieu Thanh <thanhpk@live.com>
2017-06-07 18:36:12 +0700

    add ContainString and Merge functions

--- goslice.go
+++ goslice.go
@@ -18,6 +18,35 @@ func Superset(a []string, b []string) bool {
 }
 
 // Equals compare two slice, do not care about order
-func Equals(a []string, b []string) bool {
+func Equal(a []string, b []string) bool {
 	return Superset(a, b) && Superset(b, a)
+}
+
+// ContainString check if s contains e
+func ContainString(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+// Merge merge a and b into a big slice. O(n^2)
+func Merge(s1 []string, s2 []string) []string {
+	ret := make([]string, len(s1))
+	copy(ret, s1)
+	for _, s := range s1 {
+		found := false
+		for _, subs := range s2 {
+			if subs == s {
+				found = true
+				break
+			}
+		}
+		if !found {
+			ret = append(ret, s)
+		}
+	}
+	return ret
 }
